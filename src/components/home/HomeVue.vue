217ebca9812164ddd507399bc5aedfc0ca88ec23
Quentin MOLERO <quentin.molero@outlook.com>
2022-03-29 20:42:04 +0200

    Feat(33): First routing for front with VueJS

---
+++ src/components/home/HomeVue.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Welcome to the best social media ever where you can (hopefully soon) run code :D</h1>
+</template>
+
+<script>
+export default {
+  name: "HomeVue"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/user/register/UserRegister.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Register</h1>
+</template>
+
+<script>
+export default {
+  name: "UserRegister"
+}
+</script>
+
+<style scoped>
+
+</style>
