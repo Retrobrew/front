c2a32f1a0c5830cae6db6bce87d058bed2a6dd3a
ame <amedouillard@hotmail.fr>
2022-04-09 17:24:24 +0200

    Import des composants

--- src/components/user/login/UserLogin.vue
+++ src/components/user/login/UserLogin.vue
@@ -7,9 +7,19 @@
 </template>
 
 <script lang="ts">
-import {Vue} from "vue-class-component";
+import {Options, Vue} from "vue-class-component";
+import LoginFields from "@/components/user/login/molecules/LoginFields.vue";
+import LoginRegisterRedirection from "@/components/user/login/atoms/LoginRegisterRedirection.vue";
 
+@Options ({
+  name: "UserLogin",
+  components: {
+    LoginFields,
+    LoginRegisterRedirection
+  }
+})
 export default class UserLogin extends Vue {
+
 }
 </script>
 
