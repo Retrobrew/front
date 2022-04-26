3faefae376b56effe921a777be31ed5212698204
Cédric <43600080+CedricLphn@users.noreply.github.com>
2022-04-26 17:34:39 +0200

    Revert "Feat/35/typescript setup (#23)"
    This reverts commit 3a8c1cc6fce40f60a521c1549a68b1a7f5f79def.

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,15 @@
 <template>
-  <component :is="currentView" />
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HomeVue from "@/components/home/HomeVue";
-import UserLogin from "@/components/user/login/UserLogin";
-import UserRegister from "@/components/user/register/UserRegister";
-import Error404 from "@/components/error/Error404";
-
-const routes = {
-  '/': HomeVue,
-  '/login': UserLogin,
-  '/register': UserRegister
-}
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  data() {
-    return {
-      currentPath: window.location.pathname
-    }
-  },
-  computed: {
-    currentView() {
-      return routes[this.currentPath || '/'] || Error404
-    }
-  },
-  mounted() {
-    window.addEventListener('pathnamechange', () => {
-      this.currentPath = window.location.pathname
-    })
+  components: {
+    HelloWorld
   }
 }
 </script>
@@ -39,5 +19,8 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
